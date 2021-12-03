30ac77c0bdce85b21dc97d955261095d263e7b27
Raphaël Pinel <raphaelpinel@gmail.com>
2021-12-03 11:01:32 +0200

    Add validation for tasks filters params

--- src/tasks/dto/get-tasks-filter.dto.ts
+++ src/tasks/dto/get-tasks-filter.dto.ts
@@ -1,6 +1,12 @@
+import { IsEnum, IsNotEmpty, IsOptional } from 'class-validator';
 import { TaskStatus } from '../tasks.model';
 
 export class GetTasksFilterDto {
+  @IsOptional()
+  @IsEnum(TaskStatus)
   status?: TaskStatus;
+
+  @IsOptional()
+  @IsNotEmpty()
   search?: string;
 }
